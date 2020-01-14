864da3a63defe84180cff13a617b8dd359c6da6d
Todd Traylor <toddt67878@yahoo.com>
2020-01-14 11:27:21 -0600

    Corrected Sessions Create and Destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,9 @@
 class SessionsController < ApplicationController
 
   def new
-
   end
-  
-def create
+
+  def create
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
@@ -16,10 +15,10 @@ def create
     end
   end
 
-
   def destroy
     session[:user_id] = nil
     flash[:success] = "You have successfully logged out"
     redirect_to login_path
-  end	  
+  end
+
 end
